55c798ccdf7059977de0c791ee574b8399258cbb
zager <1252496768@qq.com>
2022-04-17 14:54:45 +0800

    v1.0.0 gcc—hrm工程测试通过

---
+++ examples/ble_peripheral/ble_app_hrs/pca10040/s132/armgcc/_build/nrf52832_xxaa/ctr_drbg.c.d
@@ -0,0 +1,62 @@
+_build/nrf52832_xxaa/ctr_drbg.c.o: \
+ ../../../../../../external/mbedtls/library/ctr_drbg.c \
+ ../../../../../../external/nrf_tls/mbedtls/nrf_crypto/config/nrf_crypto_mbedtls_config.h \
+ ../config/sdk_config.h \
+ ../../../../../../external/mbedtls/include/mbedtls/check_config.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\lib\gcc\arm-none-eabi\10.2.1\include-fixed\limits.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\lib\gcc\arm-none-eabi\10.2.1\include-fixed\syslimits.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\limits.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\newlib.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\_newlib_version.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\sys\cdefs.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\machine\_default_types.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\sys\features.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\lib\gcc\arm-none-eabi\10.2.1\include\stddef.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\sys\syslimits.h \
+ ../../../../../../external/mbedtls/include/mbedtls/ctr_drbg.h \
+ ../../../../../../external/mbedtls/include/mbedtls/aes.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\lib\gcc\arm-none-eabi\10.2.1\include\stdint.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\stdint.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\sys\_intsup.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\sys\_stdint.h \
+ ../../../../../../external/mbedtls/include/mbedtls/platform_util.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\string.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\_ansi.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\sys\config.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\machine\ieeefp.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\sys\reent.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\_ansi.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\sys\_types.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\machine\_types.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\sys\lock.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\sys\string.h
+../../../../../../external/nrf_tls/mbedtls/nrf_crypto/config/nrf_crypto_mbedtls_config.h:
+../config/sdk_config.h:
+../../../../../../external/mbedtls/include/mbedtls/check_config.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\lib\gcc\arm-none-eabi\10.2.1\include-fixed\limits.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\lib\gcc\arm-none-eabi\10.2.1\include-fixed\syslimits.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\limits.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\newlib.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\_newlib_version.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\sys\cdefs.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\machine\_default_types.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\sys\features.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\lib\gcc\arm-none-eabi\10.2.1\include\stddef.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\sys\syslimits.h:
+../../../../../../external/mbedtls/include/mbedtls/ctr_drbg.h:
+../../../../../../external/mbedtls/include/mbedtls/aes.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\lib\gcc\arm-none-eabi\10.2.1\include\stdint.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\stdint.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\sys\_intsup.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\sys\_stdint.h:
+../../../../../../external/mbedtls/include/mbedtls/platform_util.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\string.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\_ansi.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\sys\config.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\machine\ieeefp.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\sys\reent.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\_ansi.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\sys\_types.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\machine\_types.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\sys\lock.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\sys\string.h:
---
+++ examples/ble_peripheral/ble_app_hrs/pca10040/s132/armgcc/_build/nrf52832_xxaa/sensorsim.c.d
@@ -0,0 +1,20 @@
+_build/nrf52832_xxaa/sensorsim.c.o: \
+ ../../../../../../components/libraries/sensorsim/sensorsim.c \
+ ../../../../../../components/libraries/sensorsim/sensorsim.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\lib\gcc\arm-none-eabi\10.2.1\include\stdint.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\stdint.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\machine\_default_types.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\sys\features.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\_newlib_version.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\sys\_intsup.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\sys\_stdint.h \
+ d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\lib\gcc\arm-none-eabi\10.2.1\include\stdbool.h
+../../../../../../components/libraries/sensorsim/sensorsim.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\lib\gcc\arm-none-eabi\10.2.1\include\stdint.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\stdint.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\machine\_default_types.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\sys\features.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\_newlib_version.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\sys\_intsup.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\arm-none-eabi\include\sys\_stdint.h:
+d\:\program\ files\ (x86)\gcc-arm-none-eabi-10-2020-q4-major-win32\tool\lib\gcc\arm-none-eabi\10.2.1\include\stdbool.h:
